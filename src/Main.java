bda26b51bd9ed20be0b50a4d965c6fd2c26cb441
MHKAPR <119296844+MhkaPR@users.noreply.github.com>
2024-07-29 12:32:38 +0330

    commit of function of print

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        int a  =0 ;
-        String s = " boo lf";
 
+
+    }
+
+    public void print(String s)
+    {
+        System.out.println(s);
     }
 }
