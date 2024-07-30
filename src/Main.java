f77e025be25e67d59abb7e8c7bf661d869040654
MHKAPR <119296844+MhkaPR@users.noreply.github.com>
2024-07-30 09:19:03 +0330

    commit of switch

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-
-   System.out.print("hello");
+    int x = 10;
+        switch (x)
+        {
+            case 1: System.out.print(1);break;
+            case 2:System.out.println(2);break;
+            default:print("hello");
+        }
     }
 
-    public void print(String s)
+    public static void print(String s)
     {
         System.out.println(s);
     }
